59d0d9ba68815578997842c7f8e69247ebbcb25d
ShamarcusW-MAC <shamarcusw84@gmail.com>
2023-02-20 15:32:28 -0500

    Initial Commit

---
+++ ReTunes_MarcSwift/ReTunes_MarcSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReTunes_MarcSwiftApp.swift
+//  ReTunes_MarcSwift
+//
+//  Created by Sha'Marcus Walker on 2/20/23.
+//
+
+import SwiftUI
+
+@main
+struct ReTunes_MarcSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
